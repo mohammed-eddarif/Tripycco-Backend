85011e916a8e7155517727815f1137c444821738
mohammed-eddarif <mohammed.eddarif06@gmail.com>
2025-03-30 23:35:33 +0000

    Initialized Spring Boot project

---
+++ src/main/java/com/tripycco/tripyccobackend/TripyccoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tripycco.tripyccobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripyccoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripyccoBackendApplication.class, args);
+    }
+
+}
